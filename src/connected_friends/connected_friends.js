430fb84f8c3f9442d698e1ff66d269e6e71855c4
Nahia Riviera <n.riviera.dev@gmail.com>
2022-07-06 11:46:16 -0500

    practice

--- src/connected_friends/connected_friends.js
+++ src/connected_friends/connected_friends.js
@@ -1,5 +1,32 @@
 const Queue = require("../lib/queue");
 
-const connected = (G, s, r) => {};
+const connected = (G, s, r) => {
+	if (Object.keys(G).length === 0) {
+		return false;
+	}
+	if (s === r) {
+		return true;
+	}
+	const enqueued = [s];
+	const discovered = new Queue();
+
+	discovered.enqueue(s);
+
+	while (discovered.first) {
+		const user = discovered.dequeue();
+		const following = G[user];
+
+		for (let followedUser of following) {
+			if (followedUser === r) {
+				return true;
+			}
+			if (!enqueued.includes(followedUser)) {
+				enqueued.push(followedUser);
+				discovered.enqueue(followedUser);
+			}
+		}
+	}
+	return false;
+};
 
 module.exports = connected;
--- src/palindromes/palindromes.js
+++ src/palindromes/palindromes.js
@@ -2,7 +2,22 @@ const Stack = require("../lib/stack");
 
 const isPalindrome = (sentence) => {
   sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-  // Your solution here
+  const middle = Math.floor(sentence.length / 2)
+  const charStack = new Stack()
+  for (let i = 0;i<middle;i++){
+    charStack.push(sentence[i])
+  }
+  let midPoint;
+  if ((sentence.length % 2) > 0){
+    midPoint = middle + 1
+  } else { midpoint = middle}
+  for (let i = midPoint;i<sentence.length;i++){
+    const stackValue = charStack.pop()
+    if (sentence[i] !== stackValue){
+      return false
+    }
+  }
+  return true
 };
-
+isPalindrome("abcba")
 module.exports = isPalindrome;
